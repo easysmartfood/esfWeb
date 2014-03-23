38c10f8bf7b4bb7d7135956f7e2d6a9cdd88f1c4
Premkumar Manivannan <premgallant@gmail.com>
2014-03-23 13:58:02 -0700

    esf web
    order alignments

--- src/com/esf/web/EsfwebServlet.java
+++ src/com/esf/web/EsfwebServlet.java
@@ -1,10 +1,10 @@
-package com.easyfood;
+package com.esf.web;
 
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class EasyfoodServlet extends HttpServlet {
+public class EsfwebServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
